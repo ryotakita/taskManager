b249b9f8d536278a6158b92d713229061830b0a4
ryota-kita <ryota-kita@kke.co.jp>
2021-07-06 16:15:07 +0900

    日付を加えれるようにした。

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,25 @@ use std::io;
 use std::process;
 use std::fmt;
 
+use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 struct Task {
     title: String,
     client: String,
+    date  : String,
     isDone: bool,
 } 
 
 impl fmt::Display for Task{
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self.isDone {
-            true  => write!(f, "☑ {} ({})", self.title, self.client),
-            false => write!(f, "  {} ({})", self.title, self.client),
+            true  => write!(f, "☑ {} ({})  - by {}", self.title, self.client, self.date),
+            false => write!(f, "  {} ({}) - by {}", self.title, self.client, self.date),
         }
     }
 }
-
 
 fn create_task_list(path: PathBuf) -> Result<Vec<Task>, Box<dyn Error>> {
     let mut rdr = csv::Reader::from_path(path);
@@ -63,7 +64,7 @@ fn add_list(lst_task: &Vec<Task>, task_add: Task, path: PathBuf) -> Vec<Task> {
     lst_task_new
 }
 
-fn create_new_task() -> Task {
+fn create_new_task() -> Result<Task, Box<dyn Error>> {
     let mut title = String::new();
     println!("input title...");
     io::stdin()
@@ -76,11 +77,25 @@ fn create_new_task() -> Task {
         .read_line(&mut client)
         .expect("msg: &str");
 
-    Task {
+    let mut date = String::new();
+    println!("input date...");
+    io::stdin()
+        .read_line(&mut date)
+        .expect("msg: &str");
+
+    let date_year: i32 = date[0..2].to_string().parse()?;
+    let date_month: u32 = date[2..4].to_string().parse()?;
+    let date_day: u32 = date[4..6].to_string().parse()?;
+
+    let date_chrono = Utc.ymd(2000 + date_year, date_month, date_day);
+    println!("{:?}", date_chrono);
+
+    Ok(Task {
         title : title.trim().to_string(),
         client: client.trim().to_string(),
+        date  : date_chrono.to_string(),
         isDone: false
-    }
+    })
 }
 
 
@@ -100,7 +115,7 @@ fn main() {
 
         match mode.trim() {
             "1" => check_list(&lst_task).expect("sorry"),
-            "2" => lst_task = add_list(&lst_task, create_new_task(), path.to_path_buf()),
+            "2" => lst_task = add_list(&lst_task, create_new_task().unwrap(), path.to_path_buf()),
             _   => println!("Your Input Number Is {}. This is Invalid", mode.trim())
         }
     }
